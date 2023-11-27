81851dc02f028194423ce1852013923cb962601b
Abdul Rauf Sultan <81006767+arsbringer65@users.noreply.github.com>
2023-11-27 13:26:28 +0800

    upadeted the menu

---
+++ src/components/menus/NaveMenu.tsx
@@ -0,0 +1,67 @@
+import {
+  IonMenu,
+  IonHeader,
+  IonToolbar,
+  IonImg,
+  IonLabel,
+  IonButtons,
+  IonButton,
+  IonIcon,
+  IonContent,
+  IonList,
+  IonItem,
+} from "@ionic/react";
+import { pencil, home, calendar, time, logOut } from "ionicons/icons";
+import React from "react";
+import UserImg from "../../assets/user.jpg";
+
+function NaveMenu(handleLogout: any) {
+  return (
+    <IonMenu contentId="phome-main" type="overlay">
+      <IonHeader>
+        <IonToolbar class="phome-menu-header">
+          <IonImg
+            src={UserImg}
+            alt="V.L.T. Hub"
+            className="phome-logocontainer"
+          />
+          <div className="phome-userinfo">
+            <IonLabel class="phome-username">Treisha Mae Monteza</IonLabel>
+            <IonButtons>
+              <IonButton className="phome-editprofile">
+                <IonIcon icon={pencil} />
+                Edit My Profile
+              </IonButton>
+            </IonButtons>
+          </div>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent className="phome-menu-content">
+        <IonList>
+          <IonItem className="phome-menu-item">
+            <IonIcon icon={home} slot="start" className="phome-menu-icon" />
+            <IonLabel class="phome-menu-label">Home</IonLabel>
+          </IonItem>
+          <IonItem className="phome-menu-item">
+            <IonIcon icon={calendar} slot="start" className="phome-menu-icon" />
+            <IonLabel class="phome-menu-label">My Events</IonLabel>
+          </IonItem>
+          <IonItem className="phome-menu-item">
+            <IonIcon icon={time} slot="start" className="phome-menu-icon" />
+            <IonLabel class="phome-menu-label">History</IonLabel>
+          </IonItem>
+        </IonList>
+        <IonItem
+          className="phome-menu-item phome-logout"
+          onClick={handleLogout}
+        >
+          <IonIcon icon={logOut} slot="start" className="phome-menu-icon" />
+          <IonLabel class="phome-menu-label">Logout</IonLabel>
+        </IonItem>
+      </IonContent>
+    </IonMenu>
+  );
+}
+
+export default NaveMenu;
--- src/pages/participant/ParticipantHomePage.tsx
+++ src/pages/participant/ParticipantHomePage.tsx
@@ -22,6 +22,7 @@ import MetaSafety from "../../assets/metasafety.jpg";
 import techythursdays from "../../assets/techythursdays.jpg";
 import Logo from "../../assets/user.jpg";
 import "./ParticipantHomePage.css";
+import NaveMenu from "../../components/menus/NaveMenu";
 
 const ParticipantHomePage: React.FC = () => {
   const handleLogout = () => {
@@ -29,55 +30,7 @@ const ParticipantHomePage: React.FC = () => {
   };
   return (
     <IonPage>
-      <IonMenu contentId="phome-main" type="overlay">
-        <IonHeader>
-          <IonToolbar class="phome-menu-header">
-            <IonImg
-              src={Logo}
-              alt="V.L.T. Hub"
-              className="phome-logocontainer"
-            />
-            <div className="phome-userinfo">
-              <IonLabel class="phome-username">Treisha Mae Monteza</IonLabel>
-              <IonButtons>
-                <IonButton className="phome-editprofile">
-                  <IonIcon icon={pencil} />
-                  Edit My Profile
-                </IonButton>
-              </IonButtons>
-            </div>
-          </IonToolbar>
-        </IonHeader>
-
-        <IonContent className="phome-menu-content">
-          <IonList>
-            <IonItem className="phome-menu-item">
-              <IonIcon icon={home} slot="start" className="phome-menu-icon" />
-              <IonLabel class="phome-menu-label">Home</IonLabel>
-            </IonItem>
-            <IonItem className="phome-menu-item">
-              <IonIcon
-                icon={calendar}
-                slot="start"
-                className="phome-menu-icon"
-              />
-              <IonLabel class="phome-menu-label">My Events</IonLabel>
-            </IonItem>
-            <IonItem className="phome-menu-item">
-              <IonIcon icon={time} slot="start" className="phome-menu-icon" />
-              <IonLabel class="phome-menu-label">History</IonLabel>
-            </IonItem>
-          </IonList>
-          <IonItem
-            className="phome-menu-item phome-logout"
-            onClick={handleLogout}
-          >
-            <IonIcon icon={logOut} slot="start" className="phome-menu-icon" />
-            <IonLabel class="phome-menu-label">Logout</IonLabel>
-          </IonItem>
-        </IonContent>
-      </IonMenu>
-
+      <NaveMenu />
       <IonHeader>
         <IonToolbar>
           <IonMenuButton slot="start" />
