3146b2b3dabb2b156ce637b1da9f0111dc58353c
Treisha Mae Monteza <87516999+orbytesss@users.noreply.github.com>
2023-12-10 17:01:16 +0800

    create the modals for the participant my events page

--- src/pages/participant/ParticipantEventPage.tsx
+++ src/pages/participant/ParticipantEventPage.tsx
@@ -1,33 +1,51 @@
 import {
   IonButton,
+  IonButtons,
   IonCard,
   IonCardContent,
   IonCardHeader,
   IonCardTitle,
   IonContent,
   IonHeader,
+  IonIcon,
+  IonImg,
   IonMenuButton,
+  IonModal,
   IonPage,
   IonSearchbar,
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { closeCircle } from "ionicons/icons";
 import React, { useState } from "react";
 import HostImg from "../../assets/host.jpg";
 import HostImg2 from "../../assets/host2.jpg";
+import MetaSafety from "../../assets/metasafety.jpg";
 import ParticipantNavMenu from "../../components/menus/ParticipantNavMenu";
 import "./ParticipantEventPage.css";
 
-const ParticipantEventPage = () => {
+const ParticipantEventPage: React.FC = () => {
   const [searchText, setSearchText] = useState("");
+  const [showModal, setShowModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState<any>(null);
+  const [showPoster, setShowPoster] = useState(false);
 
   const handleSearchChange = (e: CustomEvent) => {
     setSearchText(e.detail.value);
   };
 
+  const openModal = (event: any) => {
+    setSelectedEvent(event);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setSelectedEvent(null);
+    setShowModal(false);
+  };
   const events = [
     {
-      date: "WEDNESDAY, NOVEMBER 29, 2023",
+      date: "Wednesday, November 29, 2023",
       title:
         "Meta Safety in the Modern Age - Strategies for a Secure Digital Journey",
       hostname: "Abdul Rauf M. Sultan",
@@ -36,7 +54,7 @@ const ParticipantEventPage = () => {
       time: "1:00PM - 3:00PM",
     },
     {
-      date: "FRIDAY, DECEMBER 1, 2023",
+      date: "Friday, December 1, 2023",
       title: "#TechyThursdays - Introduction to Web 3.0",
       hostname: "Jean Irish Mer",
       hostImg: HostImg2,
@@ -92,13 +110,102 @@ const ParticipantEventPage = () => {
                   {event.time}
                 </div>
                 <div className="peventcard-btn">
-                  <IonButton className="pview-btn">View</IonButton>
+                  <IonButton
+                    className="pview-btn"
+                    onClick={() => openModal(event)}
+                  >
+                    View
+                  </IonButton>
                   <IonButton className="pcancel-btn">Cancel</IonButton>
                 </div>
               </IonCardContent>
             </IonCard>
           </React.Fragment>
         ))}
+
+        {/* Modal */}
+        <IonModal
+          isOpen={showModal}
+          onDidDismiss={() => {
+            closeModal();
+            setShowPoster(false);
+          }}
+          className="phome-modal-myevent-container"
+        >
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="end">
+                <IonButton onClick={closeModal}>
+                  <IonIcon icon={closeCircle} />
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+
+          <IonContent className="phome-modal-myevent-content">
+            {selectedEvent && (
+              <div>
+                <div className="phome-modal-header">
+                  <p className="phome-event-free">Free</p>
+                  <IonButton
+                    className="phome-view-poster-btn"
+                    onClick={() => setShowPoster(!showPoster)}
+                  >
+                    {showPoster ? "Hide Poster" : "View Poster"}
+                  </IonButton>
+                </div>
+                {showPoster && (
+                  <IonImg
+                    src={MetaSafety}
+                    alt="Poster"
+                    className="phome-poster-img"
+                  />
+                )}
+                <h2 className="phome-modal-myevent-title">
+                  {selectedEvent.title}
+                </h2>
+
+                <div className="phome-modal-host-container">
+                  <IonImg
+                    src={HostImg}
+                    alt="Abdul Rauf M. Sultan"
+                    className="phome-modal-host-img"
+                  />
+                  <p className="phome-modal-host">Abdul Rauf M. Sultan</p>
+                </div>
+                <div className="phome-modal-myevent-details">
+                  <p>
+                    <span>Description:</span> This course is designed to help
+                    you master the fundamentals of Visual C# programming. It is
+                    intended for absolute beginners with no prior programming
+                    experience. The course focuses on the fundamentals of Visual
+                    C# programming and covers the following topics: Visual C#
+                    language syntax, program structure, and implementation
+                    details.
+                  </p>
+                  <p className="pevent-modal-date">
+                    <span>Date:</span> {selectedEvent.date}
+                  </p>
+                  <p>
+                    <span>Host:</span> {selectedEvent.hostname}
+                  </p>
+                  <p>
+                    <span>Venue:</span> {selectedEvent.venue}
+                  </p>
+                  <p>
+                    <span>Time:</span> {selectedEvent.time}
+                  </p>
+                </div>
+              </div>
+            )}
+
+            <div className="phome-myevent-btn-container">
+              <IonButton expand="block" className="phome-registered-btn">
+                Registered
+              </IonButton>
+            </div>
+          </IonContent>
+        </IonModal>
       </IonContent>
     </IonPage>
   );
