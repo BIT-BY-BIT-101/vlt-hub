bb5759467ada77d10a0c3460aaa35ee937f72186
Treisha Mae Monteza <87516999+orbytesss@users.noreply.github.com>
2024-01-07 01:31:58 +0800

    added margin in the add venue page

--- src/pages/venue/VenueAddVenuePage.tsx
+++ src/pages/venue/VenueAddVenuePage.tsx
@@ -1,16 +1,17 @@
 import {
-  IonPage,
-  IonHeader,
-  IonToolbar,
-  IonButtons,
-  IonMenuButton,
-  IonTitle,
-  IonContent,
   IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
 } from "@ionic/react";
 import React from "react";
 import { useHistory } from "react-router";
 import AddVenue from "../../components/forms/AddVenue";
+import "./../../components/forms/AddVenue.css";
 
 const VenueAddVenuePage = () => {
   const history = useHistory();
